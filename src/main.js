a3a9504039963de64f3868b5b575b599eea6fc43
Ulysses Bonfim <ulysses@inplace.be>
2022-08-27 16:46:25 +0200

    banana

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import FpCookies from './components/cookie'
 import FpParams from './components/params'
 import FpValidation from './components/validation'
 
-// Wait for DOM to load before query elements
-document.addEventListener('DOMContentLoaded', () => {
+const init = () => {
     console.log('Fp: DOMContentLoaded')
     FpParams()
     FpCookies()
     FpValidation()
-})
+}
+
+// Wait for DOM to load before query elements
+// It's possible that DOMContent is already loaded, so check on document.readState
+if (document.readyState !== 'loading') init()
+else document.addEventListener('DOMContentLoaded', init)
